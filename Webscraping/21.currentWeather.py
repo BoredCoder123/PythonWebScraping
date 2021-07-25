9e03c20fe7ab7bbc02e2a00be59658dcc1c48a4d
Ankit Kathait <ankitkathait28@gmail.com>
2021-07-26 00:25:56 +0530

    Removed api id

--- Webscraping/21.currentWeather.py
+++ Webscraping/21.currentWeather.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-site = requests.get("https://api.openweathermap.org/data/2.5/weather?q=Noida&appid=9696b071fbc0027e798a790f611dd93e")
+site = requests.get("https://api.openweathermap.org/data/2.5/weather?q=Noida&appid=")
 jsonData = site.json()
 # print(jsonData)
 temp = jsonData['main']['temp']
