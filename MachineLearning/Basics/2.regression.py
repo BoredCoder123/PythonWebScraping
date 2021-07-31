880f8a3208fe4c1465a20989a017772483846b12
Ankit Kathait <ankitkathait28@gmail.com>
2021-07-31 16:10:13 +0530

    2nd lecture

---
+++ MachineLearning/Basics/2.regression.py
@@ -0,0 +1,12 @@
+import sklearn
+import quandl
+import pandas as pd
+
+df = quandl.get('WIKI/GOOGL')
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['PCT_CHG'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_CHG', 'Adj. Volume']]
+
+print(df.head())
