e05cccc23fdb28a3c5c9b728f729e5433e98e9d4
Ankit Kathait <ankitkathait28@gmail.com>
2021-07-31 16:33:00 +0530

    3rd lecture

--- MachineLearning/Basics/2.regression.py
+++ MachineLearning/Basics/2.regression.py
@@ -1,6 +1,7 @@
 import sklearn
 import quandl
 import pandas as pd
+import math
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -9,4 +10,10 @@ df['PCT_CHG'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_CHG', 'Adj. Volume']]
 
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
 print(df.head())
